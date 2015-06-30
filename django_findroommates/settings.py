73c1346d23119b22eb38f9b5d0b6d6212a78482d
Ryan <Ruikun@Ruikuns-MacBook-Pro.local>
2015-06-29 20:53:17 -0400

    move forward

--- django_findroommates/settings.py
+++ django_findroommates/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'django_findroommates.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'roomdb'
     }
 }
 
